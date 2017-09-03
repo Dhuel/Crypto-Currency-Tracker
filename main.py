8150f4c604dabd2d1b024712df1ab5bb6631c454
Dhuel <dhuelfisher@gmail.com>
2017-09-03 12:50:04 -0400

    Main starting point of code
    This is the main function of the entire code structure.
    It is the starting point for pulling the stock data which will then be stored to a database.
    This function takes two crypto currency icons, a time variable and a True value.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import organize_and_store as orgif __name__ == "__main__":    ''' This is the main function of the entire code structure.         It is the starting point for pulling the stock data which will then be stored to a database.         This function takes two crypto currency icons, a time variable and a True value.         These values are then checked against bitfinex's API     '''    org.get_stock_data("usd", 'btc', 60, True)
+import organize_and_store as org
+
+
+if __name__ == "__main__":
+    ''' This is the main function of the entire code structure.
+         It is the starting point for pulling the stock data which will then be stored to a database.
+         This function takes two crypto currency icons, a time variable and a True value.
+         These values are then checked against bitfinex's API.
+     '''
+    org.get_stock_data("usd", 'btc', 60, True)
